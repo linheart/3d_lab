88634e5cd5b6fdcea5ae27b2bcb0ec56cc8e4e2e
Alex <leshamihaylenko@bk.ru>
2024-12-01 15:42:21 +0700

    2n_ex

---
+++ 2nd_ex/hash_table/hash_table.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"hash/fnv"
+)
+
+const size = 100
+
+type HT struct {
+	node []*Node
+}
+
+type Node struct {
+	key   string
+	value string
+	next  *Node
+}
+
+func NewHT() *HT {
+	node := make([]*Node, size)
+	return &HT{node}
+}
+
+func (ht *HT) hashFunction(key string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return h.Sum32() % size
+}
+
+func (ht *HT) Get(key string) string {
+	index := ht.hashFunction(key)
+	current := ht.node[index]
+
+	for current != nil {
+		if current.key == key {
+			return current.value
+		}
+		current = current.next
+	}
+
+	return ""
+}
+
+func (ht *HT) Set(key, value string) {
+	index := ht.hashFunction(key)
+	newNode := &Node{key, value, nil}
+	newNode.next = ht.node[index]
+	ht.node[index] = newNode
+}
+
+func main() {
+	ht := NewHT()
+	ht.Set("asd", "fds")
+	fmt.Println(ht.Get("asd"))
+}
